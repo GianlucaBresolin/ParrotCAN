d3dfe2eff794d4df04de6cd22062a0af4fe1519a
Gianluca Bresolin <gianbreso02@gmail.com>
2026-04-30 19:04:54 +0200

    Add send request logic (RTR) on communication component.

--- device/communication_component/src/lib.rs
+++ device/communication_component/src/lib.rs
@@ -9,12 +9,24 @@ impl CommunicationComponent {
         Self
     }
 
-    pub fn send(&self) {
-        // write MMIO registers to send CAN frame
+    pub fn send_data(&self) {
+        // write MMIO registers to send data CAN frame (RTR = 0)
         unsafe {
             core::ptr::write_volatile(CAN_BASE.offset(0), 0x123); // ID
             core::ptr::write_volatile(CAN_BASE.offset(1), 0);     // RTR
             core::ptr::write_volatile(CAN_BASE.offset(2), 8);     // DLC
+            core::ptr::write_volatile(CAN_BASE.offset(3), 0xDE);  // Data low
+            core::ptr::write_volatile(CAN_BASE.offset(4), 0xAD);  // Data high
+            core::ptr::write_volatile(CAN_BASE.offset(5), 1);     // Command: send frame
+        }
+    }
+
+    pub fn request_data(&self) {
+        // write MMIO register to send a request frame (RTR = 1)
+        unsafe {
+            core::ptr::write_volatile(CAN_BASE.offset(0), 0x123); // ID
+            core::ptr::write_volatile(CAN_BASE.offset(1), 1);     // RTR
+            core::ptr::write_volatile(CAN_BASE.offset(2), 8);     // DLC (ignored for RTR)
             core::ptr::write_volatile(CAN_BASE.offset(3), 0xDE);  // Data low
             core::ptr::write_volatile(CAN_BASE.offset(4), 0xAD);  // Data high
             core::ptr::write_volatile(CAN_BASE.offset(5), 1);     // Command: send frame
