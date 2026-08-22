607423b6ebee9e43ad306e528b79064223c3a024
Gianluca Bresolin <gianbreso02@gmail.com>
2026-08-22 16:30:20 +0200

    [Refactor] move LOCAL_DEVICE_MANAGER inside crate::local_device.

--- device/local_device_manager/src/lib.rs
+++ device/local_device_manager/src/lib.rs
@@ -2,14 +2,16 @@
 
 pub mod device_drivers;
 
+pub static mut LOCAL_DEVICE_MANAGER: Option<LocalDeviceManager> = None;
+
 use crate::device_drivers::DeviceDriver;
 
-pub struct LocalDeviceManager<D: DeviceDriver> {
-    led_driver: D,
+pub struct LocalDeviceManager {
+    led_driver: &'static mut dyn DeviceDriver,
 }
 
-impl<D: DeviceDriver> LocalDeviceManager<D> {
-    pub fn new(led_driver: D) -> Self {
+impl LocalDeviceManager {
+    pub fn new(led_driver: &'static mut dyn DeviceDriver) -> Self {
         Self {
             led_driver
         }
@@ -17,5 +19,11 @@ impl<D: DeviceDriver> LocalDeviceManager<D> {
 
     pub fn turn_on_light(&mut self) {
         self.led_driver.turn_on();
+    }
+}
+
+pub fn init(device_driver: &'static mut dyn DeviceDriver) {
+    unsafe {
+        LOCAL_DEVICE_MANAGER = Some(LocalDeviceManager::new(device_driver));
     }
 }
